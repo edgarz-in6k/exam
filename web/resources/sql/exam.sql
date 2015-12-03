3661588e1389a86620ed950e27453e449382dc49
employee <edgarz@interlink-ua.com>
2015-12-03 10:18:19 +0200

    add mysql dump

---
+++ web/resources/sql/exam.sql
@@ -0,0 +1,141 @@
+-- MySQL dump 10.13  Distrib 5.5.46, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: Exam
+-- ------------------------------------------------------
+-- Server version	5.5.46-0ubuntu0.14.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Access`
+--
+
+DROP TABLE IF EXISTS `Access`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Access` (
+  `id_access` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_access`),
+  UNIQUE KEY `unique_id_access` (`id_access`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Access`
+--
+
+LOCK TABLES `Access` WRITE;
+/*!40000 ALTER TABLE `Access` DISABLE KEYS */;
+INSERT INTO `Access` VALUES (1,'VIEW'),(2,'EDIT');
+/*!40000 ALTER TABLE `Access` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Pages`
+--
+
+DROP TABLE IF EXISTS `Pages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Pages` (
+  `id_pages` int(11) NOT NULL AUTO_INCREMENT,
+  `path` varchar(20) NOT NULL,
+  `id_roles` int(11) NOT NULL,
+  `id_access` int(11) NOT NULL,
+  PRIMARY KEY (`id_pages`),
+  UNIQUE KEY `unique_id_pages` (`id_pages`),
+  KEY `id_roles` (`id_roles`),
+  KEY `id_access` (`id_access`),
+  CONSTRAINT `Pages_ibfk_1` FOREIGN KEY (`id_roles`) REFERENCES `Roles` (`id_roles`) ON DELETE CASCADE,
+  CONSTRAINT `Pages_ibfk_2` FOREIGN KEY (`id_access`) REFERENCES `Access` (`id_access`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Pages`
+--
+
+LOCK TABLES `Pages` WRITE;
+/*!40000 ALTER TABLE `Pages` DISABLE KEYS */;
+INSERT INTO `Pages` VALUES (1,'main.js',1,1),(2,'main.js',1,2),(3,'main.js',2,1);
+/*!40000 ALTER TABLE `Pages` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Roles`
+--
+
+DROP TABLE IF EXISTS `Roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Roles` (
+  `id_roles` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_roles`),
+  UNIQUE KEY `unique_id_roles` (`id_roles`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Roles`
+--
+
+LOCK TABLES `Roles` WRITE;
+/*!40000 ALTER TABLE `Roles` DISABLE KEYS */;
+INSERT INTO `Roles` VALUES (1,'ADMIN'),(2,'USER');
+/*!40000 ALTER TABLE `Roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Users`
+--
+
+DROP TABLE IF EXISTS `Users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Users` (
+  `id_users` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `id_roles` int(11) NOT NULL,
+  PRIMARY KEY (`id_users`),
+  UNIQUE KEY `unique_id_users` (`id_users`),
+  KEY `id_roles` (`id_roles`),
+  CONSTRAINT `Users_ibfk_1` FOREIGN KEY (`id_roles`) REFERENCES `Roles` (`id_roles`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Users`
+--
+
+LOCK TABLES `Users` WRITE;
+/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+INSERT INTO `Users` VALUES (1,'Bob','bob',1),(2,'Mark','mark',2);
+/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'Exam'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-12-03 10:15:57
